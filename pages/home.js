7ff8288b935cfe59f4bee94fee3b3727956cf960
MeYaan <jaspersasuke123@gmail.com>
2024-03-12 15:07:24 +0800

    v1.01

--- pages/home.js
+++ pages/home.js
@@ -17,6 +17,27 @@ function myFunction() {
   }
 }
 
+function toggleDarkMode() {
+  const body = document.body;
+  const darkModeIcon = document.getElementById("darkModeIcon");
+
+  // Toggle dark mode class on the body
+  body.classList.toggle("dark-mode");
+
+  // Check if dark mode is active after toggling
+  const isDarkMode = body.classList.contains("dark-mode");
+
+  // Set the mode in localStorage
+  localStorage.setItem("darkMode", isDarkMode);
+
+  // Update the dark mode icon based on the mode
+  darkModeIcon.src = isDarkMode
+    ? "../images/lightmode.png"
+    : "../images/nightmode.png";
+
+  // Additional function calls or logic after toggling
+  updateEditBioIcon();
+}
 
 
 
